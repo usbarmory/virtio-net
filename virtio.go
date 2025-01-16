023f26af1ffb3021d9472b94552b10ef202d0bc3
Andrea Barisani <andrea@inversepath.com>
2025-01-16 14:02:45 +0100

    working rx flow

--- virtio.go
+++ virtio.go
@@ -15,13 +15,21 @@ import (
 	"runtime"
 	"sync"
 
-	"github.com/usbarmory/tamago/virtio"
+	"github.com/usbarmory/tamago/kvm/virtio"
 )
 
 // VirtIO configuration
 const (
 	DeviceID   = 0x01
 	ConfigSize = 24
+)
+
+// transmit/receive queue pairs
+const (
+	// receiveq1
+	rxq = 0
+	// transmitq1
+	txq = 1
 )
 
 // Config constants
@@ -45,6 +53,28 @@ const (
 	HASH_TYPE_TCP_EX = 1 << 7
 	HASH_TYPE_UDP_EX = 1 << 8
 )
+
+// Supported Features
+const (
+	VIRTIO_NET_F_CSUM         = (1 << 0)
+	VIRTIO_NET_F_MTU          = (1 << 3)
+	VIRTIO_NET_F_MAC          = (1 << 5)
+	VIRTIO_NET_F_STATUS       = (1 << 16)
+	VIRTIO_NET_F_SPEED_DUPLEX = (1 << 63)
+
+	DriverFeatures = VIRTIO_NET_F_CSUM | VIRTIO_NET_F_MTU | VIRTIO_NET_F_MAC | VIRTIO_NET_F_STATUS | VIRTIO_NET_F_SPEED_DUPLEX
+)
+
+type VirtIONetHeader struct {
+	Flags      uint8
+	GSOType    uint8
+	HdrLen     uint16
+	GSOSize    uint16
+	CSumStart  uint16
+	CSumOffset uint16
+	// not used in legacy drivers (FIXME: detect?)
+	// NumBuffers uint16
+}
 
 type Config struct {
 	// MAC represents the interface physical address.
@@ -88,6 +118,19 @@ type Net struct {
 	tx *virtio.VirtualQueue
 }
 
+func (hw *Net) initQueue(index int, flags uint16) (queue *virtio.VirtualQueue) {
+	//size := hw.io.MaxQueueSize(index)
+	size := 8
+	length := 1518 + 12 // MTU + virtio_net_hdr (FIXME)
+
+	queue = &virtio.VirtualQueue{}
+	queue.Init(size, int(length), flags)
+
+	hw.io.SetQueueSize(index, size)
+
+	return
+}
+
 // Init initializes the VirtIO network device.
 func (hw *Net) Init() (err error) {
 	hw.Lock()
@@ -98,29 +141,20 @@ func (hw *Net) Init() (err error) {
 		ConfigSize: ConfigSize,
 	}
 
-	if err := hw.io.Init(); err != nil {
+	if err := hw.io.Init(DriverFeatures); err != nil {
 		return err
 	}
 
 	if id := hw.io.DeviceID(); id != DeviceID {
-		return fmt.Errorf("incompatible device ID (%x)", id)
+		return fmt.Errorf("incompatible device ID (%x != DeviceID)", id, DeviceID)
 	}
 
-	size := hw.io.MaxQueueSize()
-
-	if size == 0 || hw.io.QueueReady(0) || hw.io.QueueReady(1) {
+	if hw.io.QueueReady(rxq) || hw.io.QueueReady(txq) {
 		return errors.New("queues unavailable")
 	}
 
-	// receiveq1
-	hw.rx = &virtio.VirtualQueue{}
-	hw.rx.Init(size)
-	hw.io.SetQueueSize(0, size)
-
-	// transmitq1
-	hw.tx = &virtio.VirtualQueue{}
-	hw.tx.Init(size)
-	hw.io.SetQueueSize(1, size)
+	hw.rx = hw.initQueue(rxq, virtio.Write)
+	hw.tx = hw.initQueue(txq, 0)
 
 	return
 }
@@ -142,12 +176,20 @@ func (hw *Net) Config() (config Config) {
 func (hw *Net) Start(rx bool) {
 	var buf []byte
 
-	if !rx || hw.RxHandler == nil {
+	if !rx || hw.RxHandler == nil || hw.rx == nil {
 		return
 	}
 
+	hw.io.SetQueue(rxq, hw.rx)
+	hw.io.SetQueue(txq, hw.tx)
+
+	hw.io.QueueNotify(rxq)
+	hw.io.QueueNotify(txq)
+
 	for {
 		runtime.Gosched()
+
+		hw.rx.Debug()
 
 		if buf = hw.Rx(); buf != nil {
 			hw.RxHandler(buf)
@@ -158,19 +200,11 @@ func (hw *Net) Start(rx bool) {
 // Rx receives a single network frame, excluding the checksum, from the MAC
 // controller ring buffer.
 func (hw *Net) Rx() (buf []byte) {
-	hw.Lock()
-	defer hw.Unlock()
-
-	// TODO
-
-	return
+	return hw.rx.Pop()
 }
 
 // Tx transmits a single network frame, the checksum is appended automatically
 // and must not be included.
 func (hw *Net) Tx(buf []byte) {
-	hw.Lock()
-	defer hw.Unlock()
-
-	// TODO
+	hw.rx.Push(buf)
 }
